decd771c614c2d73b83cf6490b42c959e20ae97a
Georges Khalil <georgesghassankhalil@gmail.com>
2023-10-03 10:28:23 +0300

    commit six, tried ViewModel did not work, rolling back

--- app/src/main/java/com/example/myrestaurants_v1/Model/RestaurantsListViewModel.java
+++ app/src/main/java/com/example/myrestaurants_v1/Model/RestaurantsListViewModel.java
@@ -2,6 +2,17 @@ package com.example.myrestaurants_v1.Model;
 
 import androidx.lifecycle.ViewModel;
 
-public class RestaurantsListViewModel extends ViewModel {
+import java.util.ArrayList;
+import java.util.List;
 
+public class RestaurantsListViewModel extends ViewModel {
+    private List<Restaurant> restaurantList = new ArrayList<>();
+
+    public List<Restaurant> getRestaurantList() {
+        return restaurantList;
+    }
+
+    public void setRestaurantList(List<Restaurant> restaurantList) {
+        this.restaurantList = restaurantList;
+    }
 }

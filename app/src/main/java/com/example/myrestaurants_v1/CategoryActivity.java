5479f50fbcfd619fff04296b171c109c3f04ceba
Georges Khalil <georgesghassankhalil@gmail.com>
2023-11-01 14:10:03 +0200

    commit twenty-four, added option to add category and made layout for category look better

--- app/src/main/java/com/example/myrestaurants_v1/CategoryActivity.java
+++ app/src/main/java/com/example/myrestaurants_v1/CategoryActivity.java
@@ -3,6 +3,7 @@ package com.example.myrestaurants_v1;
 import static com.example.myrestaurants_v1.MyApp.restaurantDBHelper;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.myrestaurants_v1.Model.Category;
 import com.example.myrestaurants_v1.databinding.CategoryBinding;
@@ -20,5 +21,21 @@ public class CategoryActivity extends BaseActivity{
         categoryList = restaurantDBHelper.getAllCategories();
         CategoryAdapter adapter = new CategoryAdapter(this, categoryList);
         binding.categoryList.setAdapter(adapter);
+
+        binding.addCategoryButton.setOnClickListener(v -> add_category());
+    }
+
+    private void add_category() {
+        String specialty = binding.newCategoryField.getText().toString();
+        if(specialty.length() < 3){
+            binding.newCategoryField.setText("");
+            binding.newCategoryField.requestFocus();
+            Toast.makeText(this, "Enter a valid speciality", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Category category = new Category(specialty);
+        restaurantDBHelper.insertCategory(category);
+        categoryList.add(category);
+        binding.newCategoryField.setText("");
     }
 }
